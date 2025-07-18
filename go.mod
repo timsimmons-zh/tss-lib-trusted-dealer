af4833fee5f01ab17c091353fe2f4f9624635c54
timsimmons-zh <tim.simmons@zerohash.com>
2025-07-18 14:39:26 -0600

    Refactor resharing logic for ECDSA and EDDSA

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.24.5
 
 require (
 	github.com/bnb-chain/tss-lib/v2 v2.0.2
+	github.com/ethereum/go-ethereum v1.16.1
 	github.com/ipfs/go-log v1.0.5
 )
 
@@ -18,17 +19,17 @@ require (
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/holiman/uint256 v1.3.2 // indirect
 	github.com/ipfs/go-log/v2 v2.6.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/otiai10/primes v0.0.0-20210501021515-f1b2be525a11 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
-	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
 	golang.org/x/sys v0.34.0 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
 )
 
 replace github.com/agl/ed25519 => github.com/binance-chain/edwards25519 v0.0.0-20231030070956-6796d47b70ba
--- main.go
+++ main.go
@@ -10,15 +10,25 @@ import (
 	golog "github.com/ipfs/go-log"
 
 	tsscrypto "github.com/bnb-chain/tss-lib/v2/crypto"
-	"github.com/bnb-chain/tss-lib/v2/ecdsa/keygen"
-	"github.com/bnb-chain/tss-lib/v2/ecdsa/resharing"
+	eckeygen "github.com/bnb-chain/tss-lib/v2/ecdsa/keygen"
+	ecresharing "github.com/bnb-chain/tss-lib/v2/ecdsa/resharing"
+	edkeygen "github.com/bnb-chain/tss-lib/v2/eddsa/keygen"
+	edresharing "github.com/bnb-chain/tss-lib/v2/eddsa/resharing"
 	"github.com/bnb-chain/tss-lib/v2/tss"
 )
 
 // Types for channel communication
-type result struct {
+type baseResult struct {
+	pid *tss.PartyID
+}
+type ecresult struct {
 	pid  *tss.PartyID
-	data keygen.LocalPartySaveData
+	data eckeygen.LocalPartySaveData
+}
+
+type edresult struct {
+	pid  *tss.PartyID
+	data edkeygen.LocalPartySaveData
 }
 
 type msg struct {
@@ -32,6 +42,11 @@ func main() {
 		panic(err)
 	}
 
+	// runECDSAResharing()
+	runEDDSAResharing()
+}
+
+func runECDSAResharing() {
 	// 1) Define parties: importer (old group) + three co-signers (new group)
 	importerParty := tss.NewPartyID("importer", "Importer", big.NewInt(0))
 	signerParties := []*tss.PartyID{
@@ -51,12 +66,12 @@ func main() {
 
 	// 2) Generate Paillier & ZK pre-params for each party
 	fmt.Println("Computing local PreParams")
-	preImp, _ := keygen.GeneratePreParams(1 * time.Minute)
-	preSigners := make([]*keygen.LocalPreParams, 3)
+	preImp, _ := eckeygen.GeneratePreParams(1 * time.Minute)
+	preSigners := make([]*eckeygen.LocalPreParams, 3)
 	var err error
 	for i := range signerParties {
 		fmt.Printf("Computing local PreParams for signer %d\n", i)
-		preSigners[i], err = keygen.GeneratePreParams(1 * time.Minute)
+		preSigners[i], err = eckeygen.GeneratePreParams(1 * time.Minute)
 		if err != nil {
 			msg := fmt.Sprintf("failed to generate pre-params for signer %d: %v", i, err)
 			panic(msg)
@@ -67,7 +82,7 @@ func main() {
 	// Channels for messages and results
 
 	outCh := make(chan msg, 10)
-	signerEndCh := make(chan result, 3)
+	signerEndCh := make(chan ecresult, 3)
 
 	// Build resharing parameters: old=1-of-1, new=3-of-3 (t+1=3 ⇒ t=2)
 	impParams := tss.NewReSharingParameters(
@@ -82,9 +97,9 @@ func main() {
 
 	// Importer’s save data with the full private key
 	plaintextKey := big.NewInt(0xff) // ← your ECDSA private key here
-	impSave := keygen.NewLocalPartySaveData(1)
+	impSave := eckeygen.NewLocalPartySaveData(1)
 	impSave.LocalPreParams = *preImp
-	impSave.LocalSecrets = keygen.LocalSecrets{
+	impSave.LocalSecrets = eckeygen.LocalSecrets{
 		Xi:      plaintextKey,
 		ShareID: importerParty.KeyInt(),
 	}
@@ -107,18 +122,18 @@ func main() {
 	}
 
 	// Simple broadcast router: send each outgoing message to all other parties
-	partyMap := make(map[string]*resharing.LocalParty)
-	var importerPartyInstance *resharing.LocalParty
-	var signerPartyInstances [3]*resharing.LocalParty
+	partyMap := make(map[string]*ecresharing.LocalParty)
+	var importerPartyInstance *ecresharing.LocalParty
+	var signerPartyInstances [3]*ecresharing.LocalParty
 
 	// Create all parties
-	discardEndCh := make(chan result, 1) // importer results can be ignored
-	importerPartyInstance = resharing.NewLocalParty(
+	discardEndCh := make(chan ecresult, 1) // importer results can be ignored
+	importerPartyInstance = ecresharing.NewLocalParty(
 		impParams,
 		impSave,
 		makeOutCh(importerParty, outCh),
-		makeEndCh(importerParty, discardEndCh),
-	).(*resharing.LocalParty)
+		makeEcEndCh(importerParty, discardEndCh),
+	).(*ecresharing.LocalParty)
 	partyMap[importerParty.Id] = importerPartyInstance
 
 	for i, pid := range signerParties {
@@ -127,7 +142,7 @@ func main() {
 		}
 		fmt.Printf("PartyID: %s, Index: %s\n", pid.Moniker, pid.KeyInt().String())
 
-		signerSave := keygen.NewLocalPartySaveData(1)
+		signerSave := eckeygen.NewLocalPartySaveData(1)
 		signerSave.LocalPreParams = *preSigners[i]
 
 		signerSave.Ks[0] = importerParty.KeyInt()
@@ -137,12 +152,12 @@ func main() {
 		signerSave.H2j[0] = preImp.H2i
 		signerSave.PaillierPKs[0] = &preImp.PaillierSK.PublicKey
 
-		signerPartyInstances[i] = resharing.NewLocalParty(
+		signerPartyInstances[i] = ecresharing.NewLocalParty(
 			signerParams[i],
 			signerSave,
 			makeOutCh(pid, outCh),
-			makeEndCh(pid, signerEndCh),
-		).(*resharing.LocalParty)
+			makeEcEndCh(pid, signerEndCh),
+		).(*ecresharing.LocalParty)
 		partyMap[pid.Id] = signerPartyInstances[i]
 	}
 
@@ -199,7 +214,172 @@ func main() {
 	}()
 
 	// Collect each signer’s new save data (their individual share + proofs)
-	results := map[string]result{}
+	results := map[string]ecresult{}
+	for i := 0; i < 3; i++ {
+		r := <-signerEndCh
+		// fmt.Printf(">>> %s completed with result: %+v\n", r.pid.Id, r.data)
+		// Persist r.data securely for future signing
+		results[r.pid.Id] = r
+	}
+
+	wg.Wait()
+
+	// Add all the Xi to make sure they sum to importer's Xi
+	totalXi := big.NewInt(0)
+	for _, r := range results {
+		totalXi.Add(totalXi, r.data.LocalSecrets.Xi)
+		fmt.Printf(">>> %s completed with result: %+v\n", r.pid.Id, r.data)
+		fmt.Println("--------------------------------------------------------")
+		fmt.Println()
+	}
+	totalXi.Mod(totalXi, curve.Params().N) // Ensure it fits in the curve order
+	// Verify it matches the importer's original key
+	if plaintextKey.Cmp(impSave.LocalSecrets.Xi) != 0 {
+		log.Fatalf("Total Xi %s does not match importer's Xi %s", totalXi, impSave.LocalSecrets.Xi)
+	}
+	fmt.Println(">>> All signers completed successfully. Total Xi matches.")
+}
+
+func runEDDSAResharing() {
+	// 1) Define parties: importer (old group) + three co-signers (new group)
+	importerParty := tss.NewPartyID("importer", "Importer", big.NewInt(0))
+	signerParties := []*tss.PartyID{
+		tss.NewPartyID("signer1", "Signer1", big.NewInt(1)),
+		tss.NewPartyID("signer2", "Signer2", big.NewInt(2)),
+		tss.NewPartyID("signer3", "Signer3", big.NewInt(3)),
+	}
+
+	allOld := tss.NewPeerContext(
+		tss.SortPartyIDs([]*tss.PartyID{importerParty}),
+	)
+	allNew := tss.NewPeerContext(
+		tss.SortPartyIDs(signerParties),
+	)
+
+	curve := tss.Edwards() // ED25519
+
+	// Channels for messages and results
+	outCh := make(chan msg, 10)
+	signerEndCh := make(chan edresult, 3)
+
+	// Build resharing parameters: old=1-of-1, new=3-of-3 (t+1=3 ⇒ t=2)
+	impParams := tss.NewReSharingParameters(
+		curve,
+		allOld, allNew,
+		importerParty,
+		1, 0,
+		3, 2)
+
+	// impParams.NoProofFac()
+	// impParams.NoProofMod()
+
+	// Importer’s save data with the full private key
+	plaintextKey := big.NewInt(0xff) // ← your ECDSA private key here
+	impSave := edkeygen.NewLocalPartySaveData(1)
+	impSave.LocalSecrets = edkeygen.LocalSecrets{
+		Xi:      plaintextKey,
+		ShareID: importerParty.KeyInt(),
+	}
+	impSave.Ks[0] = importerParty.KeyInt()
+	impSave.BigXj[0] = tsscrypto.ScalarBaseMult(curve, plaintextKey)
+	impSave.EDDSAPub = impSave.BigXj[0]
+
+	// Set signer's resharing parameters
+	signerParams := make([]*tss.ReSharingParameters, 3)
+	for i, pid := range signerParties {
+		signerParams[i] = tss.NewReSharingParameters(curve, allOld, allNew,
+			pid, 1, 0, 3, 2)
+	}
+
+	// Simple broadcast router: send each outgoing message to all other parties
+	partyMap := make(map[string]*edresharing.LocalParty)
+	var importerPartyInstance *edresharing.LocalParty
+	var signerPartyInstances [3]*edresharing.LocalParty
+
+	// Create all parties
+	discardEndCh := make(chan edresult, 1) // importer results can be ignored
+	importerPartyInstance = edresharing.NewLocalParty(
+		impParams,
+		impSave,
+		makeOutCh(importerParty, outCh),
+		makeEdEndCh(importerParty, discardEndCh),
+	).(*edresharing.LocalParty)
+	partyMap[importerParty.Id] = importerPartyInstance
+
+	for i, pid := range signerParties {
+		if pid.KeyInt().Sign() < 0 {
+			panic(fmt.Sprintf("Invalid PartyID: %s has negative index %s", pid.Moniker, pid.KeyInt().String()))
+		}
+		fmt.Printf("PartyID: %s, Index: %s\n", pid.Moniker, pid.KeyInt().String())
+
+		signerSave := edkeygen.NewLocalPartySaveData(1)
+
+		signerSave.Ks[0] = importerParty.KeyInt()
+		signerSave.BigXj[0] = impSave.BigXj[0]
+
+		signerPartyInstances[i] = edresharing.NewLocalParty(
+			signerParams[i],
+			signerSave,
+			makeOutCh(pid, outCh),
+			makeEdEndCh(pid, signerEndCh),
+		).(*edresharing.LocalParty)
+		partyMap[pid.Id] = signerPartyInstances[i]
+	}
+
+	var wg sync.WaitGroup
+
+	// Launch each co-signer’s resharing party (they start with only pre-params)
+	for i, pid := range signerParties {
+		wg.Add(1)
+		go func(i int, pid *tss.PartyID) {
+			defer wg.Done()
+			if err := signerPartyInstances[i].Start(); err != nil {
+				panic(err)
+			}
+		}(i, pid)
+	}
+
+	// Launch importer’s party
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := importerPartyInstance.Start(); err != nil {
+			log.Fatalf("Importer resharing party failed: %v", err)
+		}
+	}()
+
+	go func() {
+		for m := range outCh {
+			payload, routing, err := m.data.WireBytes()
+			if err != nil {
+				log.Printf("Error serializing message from %s: %v", m.from.Id, err)
+				continue
+			}
+			fmt.Printf(">>> %s sending message to all parties: %s\n", m.from.Id, m.data.Type())
+			for _, to := range routing.To {
+				if to.Id == m.from.Id {
+					fmt.Printf("Ignoring message from self: %s\n", m.from.Id)
+					continue
+				}
+				p := partyMap[to.Id]
+				if p == nil {
+					log.Printf("Party instance for %s not found", to.Id)
+					continue
+				}
+				ok, err := p.UpdateFromBytes(payload, m.from, routing.IsBroadcast)
+				if err != nil {
+					log.Printf("Error updating party %s with message from %s: %v", to.Id, m.from.Id, err)
+				}
+				if !ok {
+					log.Printf("Party %s could not process message from %s: %v", to.Id, m.from.Id, err)
+				}
+				fmt.Printf(">>> %s updated party %s with message\n", m.from.Id, to.Id)
+			}
+		}
+	}()
+
+	// Collect each signer’s new save data (their individual share + proofs)
+	results := map[string]edresult{}
 	for i := 0; i < 3; i++ {
 		r := <-signerEndCh
 		// fmt.Printf(">>> %s completed with result: %+v\n", r.pid.Id, r.data)
@@ -235,11 +415,21 @@ func makeOutCh(pid *tss.PartyID, outCh chan msg) chan tss.Message {
 	}()
 	return ch
 }
-func makeEndCh(pid *tss.PartyID, endCh chan result) chan *keygen.LocalPartySaveData {
-	ch := make(chan *keygen.LocalPartySaveData, 1)
+func makeEcEndCh(pid *tss.PartyID, endCh chan ecresult) chan *eckeygen.LocalPartySaveData {
+	ch := make(chan *eckeygen.LocalPartySaveData, 1)
 	go func() {
 		for sd := range ch {
-			endCh <- result{pid: pid, data: *sd}
+			endCh <- ecresult{pid: pid, data: *sd}
+		}
+	}()
+	return ch
+}
+
+func makeEdEndCh(pid *tss.PartyID, endCh chan edresult) chan *edkeygen.LocalPartySaveData {
+	ch := make(chan *edkeygen.LocalPartySaveData, 1)
+	go func() {
+		for sd := range ch {
+			endCh <- edresult{pid: pid, data: *sd}
 		}
 	}()
 	return ch
